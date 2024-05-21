4605cf219aed991b05e9e77fc526d4f1773036c3
vansh <vanshjhamb9@gmail.com>
2024-05-21 12:47:42 +0530

    Added all

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Calculator } from "./calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Calculator App</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
